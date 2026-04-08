9c6ac69213c99ad8c308073cd9e89d9e06472852
oeungrotha11 <oeungrotha11@gmail.com>
2026-04-08 17:53:40 +0700

    save my work

---
+++ src/data/Shoes.js
@@ -0,0 +1,162 @@
+export const Shoes = [
+  {
+    id: 1,
+    name: "Nike Air Force 1",
+    brand: "Nike",
+    price: 120,
+    category: "Lifestyle",
+    image: "https://i.pinimg.com/736x/78/a7/74/78a7745a54a0d3b5d9751e300dfc5478.jpg"
+  },
+  {
+    id: 2,
+    name: "Nike Dunk Low",
+    brand: "Nike",
+    price: 110,
+    category: "Streetwear",
+    image: "https://i.pinimg.com/1200x/5a/dd/37/5add377386b0a7174f23cccd014e1490.jpg"
+  },
+  {
+    id: 3,
+    name: "Nike Air Max",
+    brand: "Nike",
+    price: 180,
+    category: "Running",
+    image: "https://i.pinimg.com/webp/1200x/e4/b4/52/e4b452e517db6aa441e89a5e1784fcff.webp"
+  },
+  {
+    id: 4,
+    name: "Nike Pegasus",
+    brand: "Nike",
+    price: 130,
+    category: "Running",
+    image: "https://i.pinimg.com/736x/08/b7/fa/08b7fa4b886929669fd677d82993e1f0.jpg"
+  },
+  {
+    id: 5,
+    name: "Nike React",
+    brand: "Nike",
+    price: 160,
+    category: "Running",
+    image: "https://i.pinimg.com/1200x/67/92/8c/67928c076f00931bd7753dc886904ada.jpg"
+  },
+  {
+    id: 6,
+    name: "Adidas Samba",
+    brand: "Adidas",
+    price: 100,
+    category: "Casual",
+    image: "https://i.pinimg.com/1200x/36/1e/2b/361e2b266fbe5e3b0f1a78aeda898a91.jpg"
+  },
+  {
+    id: 7,
+    name: "Adidas Ultraboost",
+    brand: "Adidas",
+    price: 190,
+    category: "Running",
+    image: "https://i.pinimg.com/736x/b9/63/6c/b9636cac6934cce998132ae39387283a.jpg"
+  },
+  {
+    id: 8,
+    name: "Adidas Campus",
+    brand: "Adidas",
+    price: 110,
+    category: "Streetwear",
+    image: "https://i.pinimg.com/736x/de/45/d5/de45d5eebc8e3c72850315db553da712.jpg"
+  },
+  {
+    id: 9,
+    name: "Adidas Adizero",
+    brand: "Adidas",
+    price: 255,
+    category: "Running",
+    image: "https://i.pinimg.com/1200x/6f/91/1a/6f911a661b02c7f3ac7262e7869f554d.jpg"
+  },
+  {
+    id: 10,
+    name: "Puma Suede",
+    brand: "Puma",
+    price: 85,
+    category: "Casual",
+    image: "https://i.pinimg.com/736x/ba/53/75/ba5375f841827b882399944202755759.jpg"
+  },
+  {
+    id: 11,
+    name: "Puma Nitro",
+    brand: "Puma",
+    price: 250,
+    category: "Running",
+    image: "https://i.pinimg.com/1200x/ae/06/e1/ae06e17d4367a51aa88ee5331a34b5b6.jpg"
+  },
+  {
+    id: 12,
+    name: "New Balance 550",
+    brand: "New Balance",
+    price: 120,
+    category: "Lifestyle",
+    image: "https://i.pinimg.com/736x/22/9d/91/229d91f13e2307aeea7ef85fd1541583.jpg"
+  },
+  {
+    id: 13,
+    name: "New Balance 2002R",
+    brand: "New Balance",
+    price: 150,
+    category: "Lifestyle",
+    image: "https://i.pinimg.com/736x/d3/db/92/d3db92ce16fa17fd5e6c6bddd6242e5b.jpg"
+  },
+  {
+    id: 14,
+    name: "New Balance 9060",
+    brand: "New Balance",
+    price: 180,
+    category: "Streetwear",
+    image: "https://i.pinimg.com/1200x/d9/95/ef/d995ef40e035c879ca54b663dc87b9a0.jpg"
+  },
+  {
+    id: 15,
+    name: "Converse Chuck 70",
+    brand: "Converse",
+    price: 90,
+    category: "Casual",
+    image: "https://i.pinimg.com/736x/bd/16/bc/bd16bcd15b29bac2662d46c2eff8986b.jpg"
+  },
+  {
+    id: 16,
+    name: "Vans Old Skool",
+    brand: "Vans",
+    price: 80,
+    category: "Skate",
+    image: "https://i.pinimg.com/736x/d9/91/43/d991432d4acb9da2823f45989e5bcc4f.jpg"
+  },
+  {
+    id: 17,
+    name: "Asics Gel Kayano",
+    brand: "Asics",
+    price: 180,
+    category: "Running",
+    image: "https://i.pinimg.com/736x/92/b7/07/92b70777505e55b3734da7988d33719b.jpg"
+  },
+  {
+    id: 18,
+    name: "Hoka Clifton",
+    brand: "Hoka",
+    price: 145,
+    category: "Running",
+    image: "https://i.pinimg.com/736x/16/2c/bf/162cbfa2a1b736bcd1447701d65228ad.jpg"
+  },
+  {
+    id: 19,
+    name: "On Cloudmonster",
+    brand: "On",
+    price: 170,
+    category: "Running",
+    image: "https://i.pinimg.com/1200x/e6/20/84/e620844f1c070d0ea836b59bef21014b.jpg"
+  },
+  {
+    id: 20,
+    name: "Reebok Club C",
+    brand: "Reebok",
+    price: 85,
+    category: "Casual",
+    image: "https://i.pinimg.com/736x/8f/78/1b/8f781b831ccdd7cbd16edc2720f36261.jpg"
+  }
+];
